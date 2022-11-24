3297b5510f5cebbb4c25038bc890a9d844e4b0a8
517ANT38 <102868848+517ANT38@users.noreply.github.com>
2022-11-24 21:21:19 +0400

    Translator

--- AssistantBot/AssistantBot.cs
+++ AssistantBot/AssistantBot.cs
@@ -11,17 +11,27 @@ using File = System.IO.File;
 using OpenMeteo;
 using System.Security.Cryptography;
 using System.Text;
+using GoogleApi;
+using OptionСlasses.Translator;
 
 namespace AssistantBotAPI
 {
     public class AssistantBot
     {
-        
+
 
         static async Task Main(String[] args)
         {
-            Bot bot = new Bot();
-            await bot.startBot();
+            //{
+            //Bot bot = new Bot();
+            //await bot.startBot();
+            //Translator translator = new Translator("Hello world");
+            //Console.WriteLine(translator.GetTranslation());
+            var a = new GoogleTranslate("auto", "germany");
+            //var h = new HttpClient();
+            //var a = await h.GetAsync("https://translate.google.ru/?sl=ru&tl=en&text=Привет&op=translate");
+            //var b=await a.Content.ReadAsStringAsync();
+            //Console.WriteLine(b);
 
             //    byte[] hashValue;
 
@@ -48,41 +58,39 @@ namespace AssistantBotAPI
             //    }
         }
 
+        //static async Task RunAsync()
+        //{
+        //    //    OpenMeteo.OpenMeteoClient client = new OpenMeteo.OpenMeteoClient();
 
+        //    //    //// Create new geocodingOptions object for Tokyo
+        //    //    GeocodingOptions geocodingData = new GeocodingOptions("Саратов", "ru", 1);
+
+        //    //    var apiResponse = await client.GetLocationDataAsync(geocodingData);
+        //    //    //var cityData = apiResponse.Locations[0];
+
+        //    //    //Console.WriteLine(cityData.Name + " is a city in " + cityData.Country + " with a population of " + cityData.Population + " people.");
+        //    //    //Console.WriteLine(cityData.Name + " coordinates are: " + cityData.Latitude + "/" + cityData.Longitude);
+        //    //    //OpenMeteo.OpenMeteoClient client = new OpenMeteo.OpenMeteoClient();
+
+        //    //    // Make a new api call to get the current weather in tokyo
+        //    //    WeatherForecast weatherData = await client.QueryAsync(geocodingData);
+
+        //    //    // Output the current weather to console
+        //    //    Console.WriteLine("Weather : " + weatherData.CurrentWeather.Temperature + "°C "+weatherData.CurrentWeather.Windspeed+" "+weatherData.Hourly.Precipitation[0]);
+        //    OpenMeteo.OpenMeteoClient client = new OpenMeteo.OpenMeteoClient();
+
+        //    // Set custom options
+        //    WeatherForecastOptions options = new WeatherForecastOptions();
+        //    options.Hourly = HourlyOptions.All;
+        //    options.Latitude = 35.6895f;
+        //    options.Longitude = 139.69171f; // For Tokyo
+
+        //    // Make a new api call to get the current weather in tokyo
+        //    WeatherForecast weatherData = await client.QueryAsync(options);
+
+        //    // Output the current weather to console
+        //    Console.WriteLine("Weather in Tokyo: " + weatherData.Hourly.Vapor_pressure_deficit[0] + "");
+        //}
 
     }
-    //static async Task RunAsync()
-    //{
-    //    //    OpenMeteo.OpenMeteoClient client = new OpenMeteo.OpenMeteoClient();
-
-    //    //    //// Create new geocodingOptions object for Tokyo
-    //    //    GeocodingOptions geocodingData = new GeocodingOptions("Саратов", "ru", 1);
-
-    //    //    var apiResponse = await client.GetLocationDataAsync(geocodingData);
-    //    //    //var cityData = apiResponse.Locations[0];
-
-    //    //    //Console.WriteLine(cityData.Name + " is a city in " + cityData.Country + " with a population of " + cityData.Population + " people.");
-    //    //    //Console.WriteLine(cityData.Name + " coordinates are: " + cityData.Latitude + "/" + cityData.Longitude);
-    //    //    //OpenMeteo.OpenMeteoClient client = new OpenMeteo.OpenMeteoClient();
-
-    //    //    // Make a new api call to get the current weather in tokyo
-    //    //    WeatherForecast weatherData = await client.QueryAsync(geocodingData);
-
-    //    //    // Output the current weather to console
-    //    //    Console.WriteLine("Weather : " + weatherData.CurrentWeather.Temperature + "°C "+weatherData.CurrentWeather.Windspeed+" "+weatherData.Hourly.Precipitation[0]);
-    //    OpenMeteo.OpenMeteoClient client = new OpenMeteo.OpenMeteoClient();
-
-    //    // Set custom options
-    //    WeatherForecastOptions options = new WeatherForecastOptions();
-    //    options.Hourly = HourlyOptions.All;
-    //    options.Latitude = 35.6895f;
-    //    options.Longitude = 139.69171f; // For Tokyo
-
-    //    // Make a new api call to get the current weather in tokyo
-    //    WeatherForecast weatherData = await client.QueryAsync(options);
-
-    //    // Output the current weather to console
-    //    Console.WriteLine("Weather in Tokyo: " + weatherData.Hourly.Vapor_pressure_deficit[0] + "");
-    //}
-    
 }
--- AssistantBot/OptionСlasses/Translator/Translator.cs
+++ AssistantBot/OptionСlasses/Translator/Translator.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using GTranslatorAPI;
+
 namespace OptionСlasses.Translator;
 
-public class Translator
+public class TranslatorBot
 {
+    
+    
+    private Translation objectFor;
+    public TranslatorBot(string text,string lng1,string lng2)
+    {
+        this.objectFor=new Translation();
+        this.objectFor.OriginalText=text;
+    }
+    public async Task<string> GetTranslation()
+    {
+        //HttpClient client = new HttpClient();
+        ////JsonContent content =  JsonContent.Create(objectFor);
+
+        //var json = JsonConvert.SerializeObject(objectFor);
+        //var content = new StringContent(json, Encoding.UTF8, "application/json");
+        //var response = await client.PostAsync("https://translate.argosopentech.com/translate", content);
+        //string a= await response.Content.ReadAsStringAsync();
+        //return a;
+    }
 
 }
